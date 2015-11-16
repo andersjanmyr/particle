946dd4967396a674a552bb112cc5d8006c348a82
Anders Janmyr <anders@janmyr.com>
2015-11-16 16:30:16 +0100

    Fixed initial layouting

--- main.jsx
+++ main.jsx
@@ -34,6 +34,41 @@ class Searcher extends React.Component {
   }
 }
 
+
+class BasicArticlePuff extends React.Component {
+  render() {
+    return (
+      <div className='basic-article'>
+        <h1 className='title'>
+          {this.props.article.title}
+        </h1>
+        <p className='ingress'>
+          {this.props.article.ingress}
+        </p>
+        <p className='text'>
+          {this.props.article.text}
+        </p>
+        <img className='image' src={this.props.article.images[0]} />
+      </div>
+    )
+  }
+}
+
+class ShortArticlePuff extends React.Component {
+  render() {
+    return (
+      <div className='short-article'>
+        <h1 className='title'>
+          {this.props.article.title}
+        </h1>
+        <p className='ingress'>
+          {this.props.article.ingress}
+        </p>
+      </div>
+    )
+  }
+}
+
 class Editor extends React.Component {
   constructor(props) {
     super(props);
@@ -41,20 +76,41 @@ class Editor extends React.Component {
   }
 
   render() {
+      let article = {
+        title: 'Tapirs are cool!',
+        ingress: 'Tapis have been voted the coolest animal in history',
+        text: `
+A tapir (/ˈteɪpər/ tay-pər or /təˈpɪər/ tə-peer) is a large, herbivorous mammal, similar in shape to a pig, with a short, prehensile snout. Tapirs inhabit jungle and forest regions of South America, Central America, and Southeastern Asia. The five extant species of tapirs are the Brazilian tapir, the Malayan tapir, the Baird's tapir, the kabomani tapir, and the mountain tapir. The four species that have been evaluated (the Brazilian, Malayan, Baird's and mountain tapir) are all classified as endangered or vulnerable. Their closest relatives are the other odd-toed ungulates, which include horses, donkeys, zebras and rhinoceri.
+        `,
+        images: [
+          'https://upload.wikimedia.org/wikipedia/commons/a/a1/Malayan_Tapir_Sitting.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/TapirAtSDZ.jpg/440px-TapirAtSDZ.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/2/24/Tapirbaby.jpg'
+        ]
+      };
     return (
       <div className='editor'>
-        <SearchField query={this.state.query}/>
-        <SearchResults query={this.state.results}/>
+        {this.props.puffs.map(function(Puff) {
+          console.log(Puff);
+          return React.createElement(Puff, { article: article });
+        })}
       </div>
     )
   }
 }
+
+
 class Main extends React.Component {
   render() {
+    let puffs = [
+      BasicArticlePuff,
+      ShortArticlePuff
+    ];
+
     return (
       <div>
         <Searcher/>
-        <Editor/>
+        <Editor puffs={puffs}/>
       </div>
     )
   }
