f736630e3082485fd707da343abf2a05d08d7800
Anders Janmyr <anders.janmyr@sonymobile.com>
2015-11-20 10:38:37 +0100

    Fixed article-repo tests and createId

--- test/models/article-repo-test.js
+++ test/models/article-repo-test.js
@@ -18,7 +18,7 @@ describe('ArticleRepo', function() {
         it('finds the matching articles', function(done) {
             articleRepo.find('the', function(err, models) {
                 expect(models.length).to.equal(2);
-                expect(models[0].title).to.match(/the/);
+                expect(models[0].title).to.match(/tapir/i);
                 done();
             });
         });
@@ -36,8 +36,8 @@ describe('ArticleRepo', function() {
 
         it('adds the article', function(done) {
             var len = original.length
-            articleRepo.add({title: 'Hamlet', author: 'Shakespeare'}, function(err, id) {
-                expect(id).to.equal('ham');
+            articleRepo.add({title: 'Hamlet', ingress: 'Shakespeare'}, function(err, id) {
+                expect(id).to.equal('hamlet');
                 articleRepo.find(null, function(err, models) {
                     expect(models.length).to.equal(len + 1);
                     done();
@@ -50,18 +50,17 @@ describe('ArticleRepo', function() {
                 expect(b.title).to.equal('Julius Ceasar');
                 done();
             });
-            articleRepo.add({title: 'Julius Ceasar', author: 'Shakespeare'}, function() {
+            articleRepo.add({title: 'Julius Ceasar', ingress: 'Shakespeare'}, function() {
             });
         });
         it('gets an error event if the article title exists', function(done) {
             articleRepo.on('error', function(error) {
-                expect(error).to.equal('ArticleRepo not found, id: missing');
+                expect(error).to.equal('Article already exists, id: sloths-are-slow');
                 done();
             });
-            articleRepo.remove({id: 'missing'}, function() {
+            articleRepo.add({title: 'Sloths are slow!'}, function() {
             });
         });
-
     });
 
     describe('#remove', function() {
@@ -76,7 +75,7 @@ describe('ArticleRepo', function() {
 
         it('removes the article by id', function(done) {
             var len = original.length
-            articleRepo.remove('zam', function(err) {
+            articleRepo.remove('tapirs-are-cool', function(err) {
                 articleRepo.find(null, function(err, models) {
                     expect(models.length).to.equal(len - 1);
                     done();
@@ -85,7 +84,7 @@ describe('ArticleRepo', function() {
         });
         it('removes the article by article.id', function(done) {
             var len = original.length
-            articleRepo.remove({id: 'geb'}, function(err) {
+            articleRepo.remove({id: 'tapirs-are-cool'}, function(err) {
                 articleRepo.find(null, function(err, models) {
                     expect(models.length).to.equal(len - 1);
                     done();
@@ -100,10 +99,10 @@ describe('ArticleRepo', function() {
         });
         it('sends a removed event with the article', function(done) {
             articleRepo.on('removed', function(b) {
-                expect(b.title).to.equal('Fooled by Randomness');
+                expect(b.title).to.equal('Tapirs are cool!');
                 done();
             });
-            articleRepo.remove({id: 'fbr'}, function() {
+            articleRepo.remove({id: 'tapirs-are-cool'}, function() {
             });
         });
         it('gets an error event if the article is missing', function(done) {
@@ -128,25 +127,25 @@ describe('ArticleRepo', function() {
         });
         it('calls back with error if article missing', function(done) {
             articleRepo.update({id: 'missing'}, function(err) {
-                expect(err).to.equal('ArticleRepo not found, id: missing');
+                expect(err).to.equal('Article not found, id: missing');
                 done();
             });
         });
         it('updates the article', function(done) {
             var len = original.length
-            articleRepo.update({id: 'fbr', title: 'Facebook rules!'}, function(err) {
-                articleRepo.findById('fbr', function(err, articleRepo) {
-                    expect(articleRepo.title).to.equal('Facebook rules!');
+            articleRepo.update({id: 'tapirs-are-cool', title: 'Tapirs rule!'}, function(err) {
+                articleRepo.findById('tapirs-are-cool', function(err, articleRepo) {
+                    expect(articleRepo.title).to.equal('Tapirs rule!');
                     done();
                 });
             });
         });
         it('sends an updated event with the article', function(done) {
             articleRepo.on('updated', function(b) {
-                expect(b.title).to.equal('Federal Bureau of Randomness');
+                expect(b.title).to.equal('Tapirs rule!');
                 done();
             });
-            articleRepo.update({id: 'fbr', title: 'Federal Bureau of Randomness'}, function() {
+            articleRepo.update({id: 'tapirs-are-cool', title: 'Tapirs rule!'}, function() {
             });
         });
         it('gets an error event if the article is missing', function(done) {
